5edf1f852973e2917b9ecc443d89e496c4e2db16
Stackbit Code Editor <projects@stackbit.com>
2021-04-07 07:06:53 +0000

    withPrefix.js: changed

--- src/utils/withPrefix.js
+++ src/utils/withPrefix.js
@@ -7,7 +7,7 @@ export default function withPrefix(url) {
         return url;
     }
 
-    if (_.startsWith(url, '#') || _.startsWith(url, 'http://') || _.startsWith(url, 'https://') || _.startsWith(url, 'https://')) {
+    if (_.startsWith(url, '#') || _.startsWith(url, 'http://') || _.startsWith(url, 'https://') || _.startsWith(url, 'mailto:')) {
         return url;
     }
     const basePath = _.trim(pathPrefix, '/');
